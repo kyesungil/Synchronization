0ae07b32c9d40b9e3633dbcfef71d9d972298651
kyesungil <41607872+kyesungil@users.noreply.github.com>
2020-04-01 15:48:41 +0900

    synchronizationEx1

---
+++ ThreadEx1.java
@@ -0,0 +1,44 @@
+package thread_pra;
+
+public class ThreadEx1 {
+
+	public static void main(String[] args) {
+		Runnable r = new RunnableEx21();
+		new Thread(r).start();
+		new Thread(r).start();
+
+	}
+
+}
+
+class Account {
+	private int balance  = 1000;
+	
+	public int getBalance() {
+		return balance;
+	}
+	
+	/* whitdraw()가 호출되면, 이 메서드가 종료되어 lock이 반납될때 까지 다른 쓰레드는 withdraw()가
+	 * 호출하더라도 대기상태에 머물게 된다.*/
+	public synchronized void withdraw(int money) {
+		if(balance >= money) {
+			try {
+				Thread.sleep(1000);
+			} catch(InterruptedException e) {}
+			balance -= money;
+		}
+	}
+}
+
+class RunnableEx21 implements Runnable {
+	Account acc = new Account();
+	
+	public void run() {
+		while(acc.getBalance() > 0) {
+			// 100, 200, 300중의 한값을 임으로 선택해서 출금.
+			int money = (int)(Math.random() * 3 + 1) * 100;
+			acc.withdraw(money);
+			System.out.println("balance:"+acc.getBalance());
+		}
+	}
+}
